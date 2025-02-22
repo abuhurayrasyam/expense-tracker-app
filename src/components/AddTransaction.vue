e02d9fdc855488abef1086c9ed39bcde6a4c30a1
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-02-22 20:43:29 +0600

    reponsive updated

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -40,8 +40,9 @@
 </script>
 
 <template>
-    <form @submit.prevent="addTransaction" class="form-container">
-      <div>
+    <form @submit.prevent="addTransaction">
+      <div class="form-container">
+        <div>
         <label for="title">Title</label>
         <input type="text" v-model="title" required />
       </div>
@@ -49,7 +50,9 @@
         <label for="amount">Amount</label>
         <input type="number" v-model="amount" min="0.01" step="0.01" required />
       </div>
-      <div>
+      </div>
+      <div class="form-container">
+        <div>
         <label for="type">Type</label>
         <select v-model="type" required>
           <option value="Income">Income</option>
@@ -57,6 +60,7 @@
         </select>
       </div>
       <button type="submit">Add</button>
+      </div>
     </form>
   </template>
   
